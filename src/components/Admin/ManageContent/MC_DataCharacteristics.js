e74b7c8ff3e0fc22e158d2911f0b4d3a05ee4f01
sanusi-pohlor <134247007+sanusi-pohlor@users.noreply.github.com>
2023-09-16 11:23:57 +0700

    160966.1

--- src/components/Admin/ManageContent/MC_DataCharacteristics.js
+++ src/components/Admin/ManageContent/MC_DataCharacteristics.js
@@ -1,60 +1,9 @@
-import React, { useState } from 'react';
-import { Form, Input, InputNumber, Popconfirm, Table, Typography, Button, Modal, Radio } from "antd";
+import React, { useEffect, useState } from 'react';
+import { Table, Form, Input, Button, Popconfirm, Select, Modal,InputNumber } from 'antd';
+import axios from 'axios';
 
-const CollectionCreateForm = ({ open, onCreate, onCancel }) => {
-  const [form] = Form.useForm();
-  return (
-    <Modal
-      open={open}
-      title="Create a new collection"
-      okText="Create"
-      cancelText="Cancel"
-      onCancel={onCancel}
-      onOk={() => {
-        form
-          .validateFields()
-          .then((values) => {
-            form.resetFields();
-            onCreate(values);
-          })
-          .catch((info) => {
-            console.log("Validate Failed:", info);
-          });
-      }}
-    >
-      <Form
-        form={form}
-        layout="vertical"
-        name="form_in_modal"
-        initialValues={{
-          modifier: "public",
-        }}
-      >
-        <Form.Item
-          name="data_cha_name"
-          label="ชื่อลักษณะข้อมูล"
-          rules={[
-            {
-              required: true,
-              message: "Please input the title of collection!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-      </Form>
-    </Modal>
-  );
-};
-const originData = [];
-for (let i = 0; i < 100; i++) {
-  originData.push({
-    key: i.toString(),
-    name: `Edward ${i}`,
-    age: 32,
-    address: `London Park no. ${i}`,
-  });
-}
+const { Option } = Select;
+
 const EditableCell = ({
   editing,
   dataIndex,
@@ -89,33 +38,45 @@ const EditableCell = ({
     </td>
   );
 };
+
 const MC_DataCharacteristics = () => {
-  const [open, setOpen] = useState(false);
-  const onCreate = (values) => {
-    console.log('Received values of form: ', values);
-    setOpen(false);
-  };
   const [form] = Form.useForm();
-  const [data, setData] = useState(originData);
-  const [editingKey, setEditingKey] = useState("");
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [editingKey, setEditingKey] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/ActionType_request');
+        setData(response.data); // Assuming your data is an array
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const isEditing = (record) => record.key === editingKey;
+
   const edit = (record) => {
-    form.setFieldsValue({
-      name: "",
-      age: "",
-      address: "",
-      ...record,
-    });
+    form.setFieldsValue({ ...record });
     setEditingKey(record.key);
   };
+
   const cancel = () => {
-    setEditingKey("");
+    setEditingKey('');
   };
+
   const save = async (key) => {
     try {
       const row = await form.validateFields();
       const newData = [...data];
       const index = newData.findIndex((item) => key === item.key);
+
       if (index > -1) {
         const item = newData[index];
         newData.splice(index, 1, {
@@ -123,14 +84,14 @@ const MC_DataCharacteristics = () => {
           ...row,
         });
         setData(newData);
-        setEditingKey("");
+        setEditingKey('');
       } else {
         newData.push(row);
         setData(newData);
-        setEditingKey("");
+        setEditingKey('');
       }
     } catch (errInfo) {
-      console.log("Validate Failed:", errInfo);
+      console.error('Validate Failed:', errInfo);
     }
   };
   const columns = [
@@ -147,31 +108,24 @@ const MC_DataCharacteristics = () => {
       editable: true,
     },
     {
-      title: "operation",
-      dataIndex: "operation",
+      title: 'Action',
+      dataIndex: 'operation',
+      width: '20%',
       render: (_, record) => {
         const editable = isEditing(record);
         return editable ? (
           <span>
-            <Typography.Link
-              onClick={() => save(record.key)}
-              style={{
-                marginRight: 8,
-              }}
-            >
+            <a onClick={() => save(record.key)} style={{ marginRight: 8 }}>
               Save
-            </Typography.Link>
+            </a>
             <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
               <a>Cancel</a>
             </Popconfirm>
           </span>
         ) : (
-          <Typography.Link
-            disabled={editingKey !== ""}
-            onClick={() => edit(record)}
-          >
+          <a disabled={editingKey !== ''} onClick={() => edit(record)}>
             Edit
-          </Typography.Link>
+          </a>
         );
       },
     },
@@ -192,7 +146,53 @@ const MC_DataCharacteristics = () => {
     };
   });
   return (
-    <Form form={form} component={false}>
+    <div>
+      <Button
+        type="primary"
+        onClick={() => {
+          setModalVisible(true);
+        }}
+        style={{ marginBottom: 16 }}
+      >
+        เพิ่มลักษณะข้อมูล
+      </Button>
+      <Modal
+        title="เพิ่มลักษณะข้อมูล"
+        visible={modalVisible}
+        onCancel={() => setModalVisible(false)}
+        footer={null}
+      >
+        <Form
+          form={form}
+          layout="vertical"
+          name="member_form"
+          onFinish={(values) => {
+            // Handle form submission here
+            console.log('Received form values:', values);
+            setModalVisible(false);
+          }}
+        >
+          {/* Add form fields for creating a new member */}
+          <Form.Item
+          name="data_cha_name"
+          label="ชื่อลักษณะข้อมูล"
+          rules={[
+            {
+              required: true,
+              message: "Please input the title of collection!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+          {/* Add more form fields here */}
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              เพิ่ม
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
       <Table
         components={{
           body: {
@@ -203,26 +203,12 @@ const MC_DataCharacteristics = () => {
         dataSource={data}
         columns={mergedColumns}
         rowClassName="editable-row"
+        loading={loading}
         pagination={{
           onChange: cancel,
         }}
       />
-      <Button
-        type="primary"
-        onClick={() => {
-          setOpen(true);
-        }}
-      >
-        New Collection
-      </Button>
-      <CollectionCreateForm
-        open={open}
-        onCreate={onCreate}
-        onCancel={() => {
-          setOpen(false);
-        }}
-      />
-    </Form>
+    </div>
   );
 };
 export default MC_DataCharacteristics;
--- src/components/Admin/ManageContent/MC_DetailsNotiChannels.js
+++ src/components/Admin/ManageContent/MC_DetailsNotiChannels.js
@@ -1,9 +1,51 @@
-import React, { useState } from 'react';
-import { Form, Input, InputNumber, Popconfirm, Table, Typography, Button, Modal, Select } from "antd";
+import React, { useEffect, useState } from 'react';
+import { Table, Form, Input, Button, Popconfirm, Select, Modal,InputNumber } from 'antd';
+import axios from 'axios';
+
 const { Option } = Select;
 
-const CollectionCreateForm = ({ open, onCreate, onCancel }) => {
+const EditableCell = ({
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const inputNode = inputType === "number" ? <InputNumber /> : <Input />;
+  return (
+    <td {...restProps}>
+      {editing ? (
+        <Form.Item
+          name={dataIndex}
+          style={{
+            margin: 0,
+          }}
+          rules={[
+            {
+              required: true,
+              message: `Please Input ${title}!`,
+            },
+          ]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
+    </td>
+  );
+};
+
+const MC_DetailsNotiChannels = () => {
   const [form] = Form.useForm();
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [editingKey, setEditingKey] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+
   const onGenderChange = (value) => {
     switch (value) {
       case 'male':
@@ -24,34 +66,189 @@ const CollectionCreateForm = ({ open, onCreate, onCancel }) => {
       default:
     }
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/ActionType_request');
+        setData(response.data); // Assuming your data is an array
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const isEditing = (record) => record.key === editingKey;
+
+  const edit = (record) => {
+    form.setFieldsValue({ ...record });
+    setEditingKey(record.key);
+  };
+
+  const cancel = () => {
+    setEditingKey('');
+  };
+
+  const save = async (key) => {
+    try {
+      const row = await form.validateFields();
+      const newData = [...data];
+      const index = newData.findIndex((item) => key === item.key);
+
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, {
+          ...item,
+          ...row,
+        });
+        setData(newData);
+        setEditingKey('');
+      } else {
+        newData.push(row);
+        setData(newData);
+        setEditingKey('');
+      }
+    } catch (errInfo) {
+      console.error('Validate Failed:', errInfo);
+    }
+  };
+  const columns = [
+    {
+      title: 'รหัสรายละเอียดช่องทางการแจ้ง',
+      dataIndex: 'dnc_id',
+      width: '20%',
+      editable: true,
+    },
+    {
+      title: 'รหัสช่องทางสื่อ',
+      dataIndex: 'dnc_med_id',
+      width: '60%',
+      editable: true,
+    },
+    {
+      title: 'รหัสการแจ้ง',
+      dataIndex: 'dnc_info_id',
+      width: '60%',
+      editable: true,
+    },
+    {
+      title: 'รหัสผู้เผยแพร',
+      dataIndex: 'dnc_pub_id',
+      width: '60%',
+      editable: true,
+    },
+    {
+      title: 'รหัสรูปแบบข้อมูล',
+      dataIndex: 'dnc_fm_d_id',
+      width: '60%',
+      editable: true,
+    },
+    {
+      title: 'รหัสการจัดการ',
+      dataIndex: 'dnc_prob_id',
+      width: '60%',
+      editable: true,
+    },
+    {
+      title: 'ขอบเขตการเผยแพร',
+      dataIndex: 'dnc_scop_pub',
+      width: '60%',
+      editable: true,
+    },
+    {
+      title: 'จำนวนสมาชิกในกลุ่มที่อยู่ในสื่อ',
+      dataIndex: 'dnc_num_mem_med',
+      width: '60%',
+      editable: true,
+    },
+    {
+      title: 'วันที่ในสื่อ',
+      dataIndex: 'dnc_date_med',
+      width: '60%',
+      editable: true,
+    },
+    {
+      title: 'ภาพ capture',
+      dataIndex: 'dnc_capt',
+      width: '60%',
+      editable: true,
+    },
+    {
+      title: 'Link URL',
+      dataIndex: 'dnc_link',
+      width: '60%',
+      editable: true,
+    },
+    {
+      title: 'Action',
+      dataIndex: 'operation',
+      width: '20%',
+      render: (_, record) => {
+        const editable = isEditing(record);
+        return editable ? (
+          <span>
+            <a onClick={() => save(record.key)} style={{ marginRight: 8 }}>
+              Save
+            </a>
+            <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+              <a>Cancel</a>
+            </Popconfirm>
+          </span>
+        ) : (
+          <a disabled={editingKey !== ''} onClick={() => edit(record)}>
+            Edit
+          </a>
+        );
+      },
+    },
+  ];
+  const mergedColumns = columns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        inputType: col.dataIndex === "age" ? "number" : "text",
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record),
+      }),
+    };
+  });
   return (
-    <Modal
-      open={open}
-      title="Create a new collection"
-      okText="Create"
-      cancelText="Cancel"
-      onCancel={onCancel}
-      onOk={() => {
-        form
-          .validateFields()
-          .then((values) => {
-            form.resetFields();
-            onCreate(values);
-          })
-          .catch((info) => {
-            console.log("Validate Failed:", info);
-          });
-      }}
-    >
-      <Form
-        form={form}
-        layout="vertical"
-        name="form_in_modal"
-        initialValues={{
-          modifier: "public",
+    <div>
+      <Button
+        type="primary"
+        onClick={() => {
+          setModalVisible(true);
         }}
+        style={{ marginBottom: 16 }}
       >
-        <Form.Item
+        เพิ่มช่องทางสื่อ
+      </Button>
+      <Modal
+        title="เพิ่มช่องทางสื่อ"
+        visible={modalVisible}
+        onCancel={() => setModalVisible(false)}
+        footer={null}
+      >
+        <Form
+          form={form}
+          layout="vertical"
+          name="member_form"
+          onFinish={(values) => {
+            // Handle form submission here
+            console.log('Received form values:', values);
+            setModalVisible(false);
+          }}
+        >
+          {/* Add form fields for creating a new member */}
+          <Form.Item
           name="dnc_med_id"
           label="รหัสช่องทางสื่อ"
           rules={[
@@ -215,211 +412,14 @@ const CollectionCreateForm = ({ open, onCreate, onCancel }) => {
         >
           <Input />
         </Form.Item>
-      </Form>
-    </Modal>
-  );
-};
-const originData = [];
-for (let i = 0; i < 100; i++) {
-  originData.push({
-    key: i.toString(),
-    name: `Edward ${i}`,
-    age: 32,
-    address: `London Park no. ${i}`,
-  });
-}
-const EditableCell = ({
-  editing,
-  dataIndex,
-  title,
-  inputType,
-  record,
-  index,
-  children,
-  ...restProps
-}) => {
-  const inputNode = inputType === "number" ? <InputNumber /> : <Input />;
-  return (
-    <td {...restProps}>
-      {editing ? (
-        <Form.Item
-          name={dataIndex}
-          style={{
-            margin: 0,
-          }}
-          rules={[
-            {
-              required: true,
-              message: `Please Input ${title}!`,
-            },
-          ]}
-        >
-          {inputNode}
-        </Form.Item>
-      ) : (
-        children
-      )}
-    </td>
-  );
-};
-const MC_DetailsNotiChannels = () => {
-  const [open, setOpen] = useState(false);
-  const onCreate = (values) => {
-    console.log('Received values of form: ', values);
-    setOpen(false);
-  };
-  const [form] = Form.useForm();
-  const [data, setData] = useState(originData);
-  const [editingKey, setEditingKey] = useState("");
-  const isEditing = (record) => record.key === editingKey;
-  const edit = (record) => {
-    form.setFieldsValue({
-      name: "",
-      age: "",
-      address: "",
-      ...record,
-    });
-    setEditingKey(record.key);
-  };
-  const cancel = () => {
-    setEditingKey("");
-  };
-  const save = async (key) => {
-    try {
-      const row = await form.validateFields();
-      const newData = [...data];
-      const index = newData.findIndex((item) => key === item.key);
-      if (index > -1) {
-        const item = newData[index];
-        newData.splice(index, 1, {
-          ...item,
-          ...row,
-        });
-        setData(newData);
-        setEditingKey("");
-      } else {
-        newData.push(row);
-        setData(newData);
-        setEditingKey("");
-      }
-    } catch (errInfo) {
-      console.log("Validate Failed:", errInfo);
-    }
-  };
-  const columns = [
-    {
-      title: 'รหัสรายละเอียดช่องทางการแจ้ง',
-      dataIndex: 'dnc_id',
-      width: '20%',
-      editable: true,
-    },
-    {
-      title: 'รหัสช่องทางสื่อ',
-      dataIndex: 'dnc_med_id',
-      width: '60%',
-      editable: true,
-    },
-    {
-      title: 'รหัสการแจ้ง',
-      dataIndex: 'dnc_info_id',
-      width: '60%',
-      editable: true,
-    },
-    {
-      title: 'รหัสผู้เผยแพร',
-      dataIndex: 'dnc_pub_id',
-      width: '60%',
-      editable: true,
-    },
-    {
-      title: 'รหัสรูปแบบข้อมูล',
-      dataIndex: 'dnc_fm_d_id',
-      width: '60%',
-      editable: true,
-    },
-    {
-      title: 'รหัสการจัดการ',
-      dataIndex: 'dnc_prob_id',
-      width: '60%',
-      editable: true,
-    },
-    {
-      title: 'ขอบเขตการเผยแพร',
-      dataIndex: 'dnc_scop_pub',
-      width: '60%',
-      editable: true,
-    },
-    {
-      title: 'จำนวนสมาชิกในกลุ่มที่อยู่ในสื่อ',
-      dataIndex: 'dnc_num_mem_med',
-      width: '60%',
-      editable: true,
-    },
-    {
-      title: 'วันที่ในสื่อ',
-      dataIndex: 'dnc_date_med',
-      width: '60%',
-      editable: true,
-    },
-    {
-      title: 'ภาพ capture',
-      dataIndex: 'dnc_capt',
-      width: '60%',
-      editable: true,
-    },
-    {
-      title: 'Link URL',
-      dataIndex: 'dnc_link',
-      width: '60%',
-      editable: true,
-    },
-    {
-      title: "operation",
-      dataIndex: "operation",
-      render: (_, record) => {
-        const editable = isEditing(record);
-        return editable ? (
-          <span>
-            <Typography.Link
-              onClick={() => save(record.key)}
-              style={{
-                marginRight: 8,
-              }}
-            >
-              Save
-            </Typography.Link>
-            <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
-              <a>Cancel</a>
-            </Popconfirm>
-          </span>
-        ) : (
-          <Typography.Link
-            disabled={editingKey !== ""}
-            onClick={() => edit(record)}
-          >
-            Edit
-          </Typography.Link>
-        );
-      },
-    },
-  ];
-  const mergedColumns = columns.map((col) => {
-    if (!col.editable) {
-      return col;
-    }
-    return {
-      ...col,
-      onCell: (record) => ({
-        record,
-        inputType: col.dataIndex === "age" ? "number" : "text",
-        dataIndex: col.dataIndex,
-        title: col.title,
-        editing: isEditing(record),
-      }),
-    };
-  });
-  return (
-    <Form form={form} component={false}>
+          {/* Add more form fields here */}
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              เพิ่ม
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
       <Table
         components={{
           body: {
@@ -430,26 +430,12 @@ const MC_DetailsNotiChannels = () => {
         dataSource={data}
         columns={mergedColumns}
         rowClassName="editable-row"
+        loading={loading}
         pagination={{
           onChange: cancel,
         }}
       />
-      <Button
-        type="primary"
-        onClick={() => {
-          setOpen(true);
-        }}
-      >
-        New Collection
-      </Button>
-      <CollectionCreateForm
-        open={open}
-        onCreate={onCreate}
-        onCancel={() => {
-          setOpen(false);
-        }}
-      />
-    </Form>
+    </div>
   );
 };
 export default MC_DetailsNotiChannels;
